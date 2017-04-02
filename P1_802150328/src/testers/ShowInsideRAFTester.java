de43c1e588ab23753fc3874aabfbd364abe3f659
jahdiel <jahdiel.alvarez@upr.edu>
2017-04-01 21:07:48 -0400

    Generalized the usage of the tester. Accepts multiple blocksizes and capacities.

--- P1_802150328/src/testers/ShowInsideRAFTester.java
+++ P1_802150328/src/testers/ShowInsideRAFTester.java
@@ -28,10 +28,11 @@ public class ShowInsideRAFTester {
 	}
 	
 	public static void ShowBlockZero(DiskUnit d) {
+		int numOfInt = d.getBlockSize() / 4;
 		try {
 			d.disk.seek(0);
 			System.out.print("Block 0: ");
-			for (int i=0; i < 8; i++) {
+			for (int i=0; i < numOfInt; i++) {
 				int num = d.disk.readInt();
 				System.out.print(num+" ");
 			}
@@ -43,8 +44,10 @@ public class ShowInsideRAFTester {
 	
 	public static void ShowINodeBlocks(DiskUnit d) {
 		int count = 1;
-		int bytePos = 32;
+		int blockSize = d.getBlockSize();
+		int bytePos = blockSize;
 		int numOfBlocks = (int) Math.ceil((d.getiNodeNum() * 9.0) / d.getBlockSize()) + 1;
+		int nodePerBlock = blockSize / 9;
 
 		try {
 			d.disk.seek(bytePos);
@@ -52,13 +55,13 @@ public class ShowInsideRAFTester {
 				System.out.println();
 				System.out.print("Block "+count+": ");
 				count++;	
-				for (int j=0; j < 3; j++) {
+				for (int j=0; j < nodePerBlock; j++) {
 					int num = d.disk.readInt();
 					int size = d.disk.readInt();
 					boolean type = d.disk.readBoolean();
 					System.out.print(num+" "+size+" "+type+" ");	
 				}
-				bytePos += 32;
+				bytePos += blockSize;
 				d.disk.seek(bytePos);
 			}
 		} catch (IOException e) {
@@ -69,20 +72,22 @@ public class ShowInsideRAFTester {
 	
 	public static void ShowDataBlocks(DiskUnit d) {	
 		int numOfBlocks = (int) Math.ceil((d.getiNodeNum() * 9.0) / d.getBlockSize()) + 1;
-		int bytePos = 32 * numOfBlocks;
+		int blockSize = d.getBlockSize();
+		int bytePos = blockSize * numOfBlocks;
 		int count = numOfBlocks;
 		int capacity = d.getCapacity();
+		int numOfInt = blockSize / 4;
 		try {
 			d.disk.seek(bytePos);
 			for (int i=1; i < capacity-numOfBlocks+1; i++) {
 				System.out.println();
 				System.out.print("Block "+count+": ");
 				count++;	
-				for (int j=0; j < 8; j++) {
+				for (int j=0; j < numOfInt; j++) {
 					int num = d.disk.readInt();
 					System.out.print(num+" ");
 				}
-				bytePos += 32;
+				bytePos += blockSize;
 				d.disk.seek(bytePos);
 			}
 		} catch (IOException e) {
